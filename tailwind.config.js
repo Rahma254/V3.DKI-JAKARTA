57fdb40fc5bd5d59410ad4e21f1e65f3345cc47d
Rahma254 <rahmasiska661@gmail.com>
2025-07-11 15:43:43 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jakarta-primary': '#2C3E50',
+        'jakarta-secondary': '#34495E'
+      }
+    }
+  },
+  plugins: []
+}￼Enter￼Enter
