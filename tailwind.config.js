12016b6f77592a81c8e4d459bedc5dcdf8da564c
Rahma254 <rahmasiska661@gmail.com>
2025-07-11 16:42:59 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
+// File: tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Ini akan memindai semua file di dalam folder src
   ],
   theme: {
-    extend: {
-      colors: {
-        'jakarta-primary': '#2C3E50',
-        'jakarta-secondary': '#34495E'
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}￼Enter￼Enter
+  plugins: [],
+}
